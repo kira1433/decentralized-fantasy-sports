f8f0c36c2d28b55801aaa954d9efd8047da89976
kira1433 <krovidiaashishchandra@gmail.com>
2024-04-11 17:28:00 +0530

    Fixed server for matches

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import json
 
 if __name__ == '__main__':
     with open("data.json", "w") as file:
-        json.dump({"transactions": [], "users": []}, file)
+        json.dump({"transactions": [], "users": [], "matches": []}, file)
     input()
     with open("data.json", "w") as file:
-        json.dump({"transactions": [], "users": []}, file)
+        json.dump({"transactions": [], "users": [], "matches": []}, file)
