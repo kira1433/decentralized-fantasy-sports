49e205fd782e9fe9fc45061648cd0b0f706fb200
vigneshreddy <vrtarimala@gmail.com>
2024-04-13 00:42:52 +0530

    hmac creation and verification

---
+++ modules/hmac.py
@@ -0,0 +1,24 @@
+import hmac 
+import hashlib
+from modules.user import Users
+
+def create_hmac(secret_key,message):
+    print(f"secret: {secret_key}, message: {message}")
+    hmac_obj = hmac.new(secret_key.encode(), msg=message.encode(), digestmod=hashlib.sha256)
+    
+    return hmac_obj.hexdigest()
+
+def verify_hmac(secret_key,message,received_hmac):
+    expected_hmac=create_hmac(secret_key=secret_key,message=message)
+    return hmac.compare_digest(received_hmac, expected_hmac)
+
+def verify_transaction_hmac(transaction):
+    secret_key="#"
+    if transaction.user=="genesis":
+        secret_key=Users.fetch_secret_key(transaction.match)
+    else:
+        secret_key=Users.fetch_secret_key(transaction.user)
+    transaction_encoded=transaction.user+transaction.team+transaction.match+str(transaction.amount)
+    received_hmac=transaction.transaction_hash
+    return verify_hmac(secret_key=secret_key,message=transaction_encoded,received_hmac=received_hmac)
+    
